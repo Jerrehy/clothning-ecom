696ffaf8b0e2af31358604aac29d76bf3e40ebcf
Jerrehy <sacha15012000@gmail.com>
2023-06-04 22:57:22 +0300

    Renew settings ver4

--- ecom/settings.py
+++ ecom/settings.py
@@ -4,8 +4,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_ORIGINS = ['http://*', 'https://*']
-CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()
+CSRF_TRUSTED_ORIGINS = ['https://web-production-d562.up.railway.app']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
