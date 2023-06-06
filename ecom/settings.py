6fb6ccf88b9f95314b4a5755dd0e31c2a63f5715
Jerrehy <sacha15012000@gmail.com>
2023-06-06 23:33:57 +0300

    Renew database

--- ecom/settings.py
+++ ecom/settings.py
@@ -70,8 +70,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'lLeHR8urwueqn9jvD6eb',
+        'HOST': 'containers-us-west-103.railway.app',
+        'PORT': '7546',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
